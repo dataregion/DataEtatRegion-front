315d671aebb7e9e816cf3854b80937828c351c7b
Romain Gautier <romain.gautier@sib.fr>
2023-09-20 15:31:17 +0200

    [FIX] fix mock_api

--- e2e/tests/utils/mock-api.ts
+++ e2e/tests/utils/mock-api.ts
@@ -3,18 +3,37 @@ import { default as financial } from '../../mock-data/financial-data/financial.j
 
 import { Page } from '@playwright/test';
 
+function is_url_ae_annees(url: URL) {
+  return url.pathname == '/financial-data/api/v1/ae/annees'
+}
+
+function is_url_budget_programmes(url: URL) {
+  return url.pathname == '/budget/api/v1/programme'
+}
+
+function is_url_ae(url: URL) {
+  return url.pathname == '/financial-data/api/v1/ae'
+}
+
 async function mockRefApi(page: Page) {
   await page.route(
-    /.*\/budget\/api\/v1\/programme.*/,
+    is_url_budget_programmes,
     async (route: any) => {
       const json = programmes;
       await route.fulfill({ json });
     }
   );
 
+  await page.route(
+    is_url_ae_annees,
+    async (route: any) => {
+      const json = [2023];
+      await route.fulfill({ json });
+    }
+  )
 
   await page.route(
-    /.*\/financial-data\/api\/v1\/ae.*/,
+    is_url_ae,
     async (route: any) => {
       const json = financial;
       await route.fulfill({ json });
