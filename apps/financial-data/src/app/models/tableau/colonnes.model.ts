b018d341e1fe471c9c35beefeff5f1df36dbddea
Benjamin Bagot <benjamin.bagot@sib.fr>
2023-09-29 15:18:37 +0200

    [FEAT] Nouvelle colonne Date création EJ

--- apps/financial-data/src/app/models/tableau/colonnes.model.ts
+++ apps/financial-data/src/app/models/tableau/colonnes.model.ts
@@ -138,6 +138,13 @@ export const colonnes: ColumnMetaDataDef[] = [
         renderFn: (row, col) =>
             row[col.name] ? new Date(row[col.name]).toLocaleString([], { year: 'numeric', month: 'numeric', day: 'numeric' }) : '',
     },
+    {
+        name: 'date_replication',
+        label: 'Date création EJ',
+        displayed: false,
+        renderFn: (row, col) =>
+            row[col.name] ? new Date(row[col.name]).toLocaleString([], { year: 'numeric', month: 'numeric', day: 'numeric' }) : '',
+    },
 ]
 
 
