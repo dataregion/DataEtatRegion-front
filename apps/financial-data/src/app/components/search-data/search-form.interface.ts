41a4adab1be3a5ea580d3d33f899b83622425ca5
Benjamin Bagot <benjamin.bagot@sib.fr>
2023-10-23 17:12:30 +0200

    [FEAT] Ajout select des types de bénéficiaires

--- apps/financial-data/src/app/components/search-data/search-form.interface.ts
+++ apps/financial-data/src/app/components/search-data/search-form.interface.ts
@@ -2,6 +2,7 @@ import { FormControl } from "@angular/forms";
 import { Beneficiaire } from "@models/search/beneficiaire.model";
 import { Bop } from "@models/search/bop.model";
 import { TypeLocalisation } from "apps/common-lib/src/lib/models/geo.models";
+import { SearchTypeCategorieJuridique } from "apps/common-lib/src/lib/services/interface-data.service";
 import { TagFieldData } from "./tags-field-data.model";
 
 export interface SearchForm {
@@ -11,5 +12,6 @@ export interface SearchForm {
     location: FormControl<any>;
     year: FormControl<number[] | null>;
     beneficiaires: FormControl<Beneficiaire[] | null>;
+    types_beneficiaires: FormControl<SearchTypeCategorieJuridique[] | null>;
     tags: FormControl<TagFieldData[] | null>
 }
