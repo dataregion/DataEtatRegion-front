a7b5a98049e12b0c9d2feeeb1e543b9ef6a74e47
Romain Gautier <romain.gautier@sib.fr>
2024-02-06 10:22:25 +0100

    [FEAT] colonnes.model.ts pour france relance

---
+++ apps/france-relance/src/app/models/tableau/colonnes.model.ts
@@ -0,0 +1,46 @@
+import { AggregatorFns, ParameterizedColumnMetaDataDef, RowData } from "apps/grouping-table/src/lib/components/grouping-table/group-utils";
+
+const moneyFormat = new Intl.NumberFormat('fr-FR', {
+  style: 'currency',
+  currency: 'EUR',
+});
+
+export type LaureatColumnMetaDataDef = ParameterizedColumnMetaDataDef<RowData>
+
+export const colonnes: LaureatColumnMetaDataDef[] = [
+      { name: 'Structure', label: 'Lauréat' },
+      {
+        name: 'SubventionAccordée',
+        label: 'Montant',
+        renderFn: (row, col) =>
+          row[col.name] ? moneyFormat.format(row[col.name]) : row[col.name],
+        aggregateReducer: AggregatorFns.sum,
+        aggregateRenderFn: (aggregateValue) =>
+          aggregateValue ? moneyFormat.format(aggregateValue) : aggregateValue,
+        columnStyle: {
+          'text-align': 'right',
+          'min-width': '16ex',
+          'flex-grow': '0',
+        },
+      },
+      {
+        name: 'axe',
+        label: 'Axe',
+      },
+      {
+        name: 'sous-axe',
+        label: 'Sous Axe',
+      },
+      {
+        name: 'dispositif',
+        label: 'Dispositif',
+      },
+      {
+        name: 'territoire',
+        label: 'Territoire',
+      },
+      {
+        name: 'Synthèse',
+        label: 'Synthèse',
+      },
+]
