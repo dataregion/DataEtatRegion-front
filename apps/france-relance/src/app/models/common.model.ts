a135cf6f42f12268c66a4b84581b7e23ffe65166
Romain Gautier <romain.gautier@sib.fr>
2024-02-06 12:01:33 +0100

    [FIX] desactivation de regle linter - bug linter?

--- apps/france-relance/src/app/models/common.model.ts
+++ apps/france-relance/src/app/models/common.model.ts
@@ -1,5 +1,8 @@
 /** Représente la source de données des lauréats */
 export enum SourceLaureatsData {
+    // XXX: bug linter ? la variable est bel et bien utilisée
+    // eslint-disable-next-line no-unused-vars
     RELANCE = "RELANCE",
+    // eslint-disable-next-line no-unused-vars
     FRANCE2030 = "FRANCE_2030",
 }
