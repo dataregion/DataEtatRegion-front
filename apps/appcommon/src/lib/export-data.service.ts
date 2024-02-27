52ea99867f5876c47f18db4e74c02ffb3f88bb02
Romain Gautier <romain.gautier@sib.fr>
2024-02-27 15:29:00 +0100

    [CHORE] creation appcommon

---
+++ apps/appcommon/src/lib/export-data.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from "@angular/core";
+import { ExportableAsJson } from "apps/common-lib/src/lib/models/exportable-as-json.model";
+import { JSONObject } from "apps/common-lib/src/lib/models/jsonobject";
+import { DisplayedOrderedColumn } from "apps/grouping-table/src/lib/components/grouping-table/group-utils";
+
+import * as XLSX from 'xlsx';
+
+/**
+ * Utilitaires pour l'export de données sous différents formats
+ */
+@Injectable({
+    'providedIn': 'root'
+})
+export class ExportDataService {
+    
+  /**
+   * Création d'un Blob à partir de données exportables
+   * @param data Données exportables
+   * @param ext Extension désirée pour le fichier qui contiendra le Blob
+   * @param columns Colonnes des données à insérer dans le Blob
+   * @returns 
+   */
+  public getBlob(data: ExportableAsJson[], ext: string, columns: DisplayedOrderedColumn[] | null): Blob | null {
+    // Transformation des données financières en JSON
+    const jsonData = [];
+    for (const item of data) {
+      let object = item.exportAsJson();
+      // Si des colonnes ont été précisées
+      if (columns) {
+        // Suppression des colonnes non-affichées de l'objet JSON
+        Object.keys(object).forEach(c => {
+          if (!columns.map(c => c.columnLabel).includes(c)) {
+            delete object[c];
+          }
+        });
+        columns.filter(c => 'displayed' in c && !c.displayed).map(c => c.columnLabel).forEach(c => {
+          delete object[c];
+        });
+        // Ordre des données du JSON en fonction de l'ordre des colonnes
+        object = Object.keys(object).sort((col1, col2) => {
+          const index1 = columns.findIndex((col) => col.columnLabel === col1)
+          const index2 = columns.findIndex((col) => col.columnLabel === col2)
+          return index1 - index2;
+        }).reduce((obj:JSONObject, key: string) => { 
+          obj[key] = object[key]; 
+          return obj;
+        }, {});
+      }
+      jsonData.push(object);
+    }
+
+    // Construction du Blob
+    let buffer: any = null;
+    let mimetype: string = "";
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(jsonData);
+    if (ext == "csv") {
+      buffer = XLSX.utils.sheet_to_csv(worksheet);
+      mimetype = "text/csv"
+    } else if (ext == "xlsx") {
+      const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+      buffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+      mimetype = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    } else if (ext == "ods") {
+      const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+      buffer = XLSX.write(workbook, { bookType: 'ods', type: 'array' });
+      mimetype = "application/vnd.oasis.opendocument.spreadsheet";
+    }
+    return buffer && mimetype ? new Blob([buffer], { type: "{{mimetype}};charset=utf-8;" }) : null;
+    }
+}
