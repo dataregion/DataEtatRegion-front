a5649831aef1b891ba79f3414cb833e446812e1d
Sylvain Jolivet <sylvain.jolivet@sib.fr>
2025-03-27 11:26:42 +0100

    changement config eslint et ajout de librairie

---
+++ eslint.config.mjs
@@ -0,0 +1,114 @@
+import globals from "globals";
+import angularEslintTemplate from "@angular-eslint/eslint-plugin-template";
+import parser from "@angular-eslint/template-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    {
+        ignores: ["nginx/**/*", '**/eslint.config.mjs'],
+    },
+    ...tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                document: true,
+                window: true,
+            },
+            ecmaVersion: 5,
+            sourceType: "commonjs",
+            parserOptions: {
+                project: "tsconfig.json",
+                tsconfigRootDir: __dirname,
+            },
+        },
+    },
+    ...compat.extends("plugin:storybook/recommended").map(config => ({
+        ...config,
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                document: true,
+                window: true,
+            },
+        },
+    })),
+    ...compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@angular-eslint/recommended",
+        "plugin:@angular-eslint/template/process-inline-templates"
+    ).map(config => ({
+        ...config,
+        files: ["**/*.ts"],
+    })),
+    {
+        files: ["**/*.ts"],
+
+        languageOptions: {
+            sourceType: "script",
+
+            parserOptions: {
+                project: true,
+                createDefaultProgram: true,
+                tsconfigRootDir: __dirname,
+            },
+        },
+
+        rules: {
+            "@angular-eslint/prefer-standalone": "off",
+
+            "no-irregular-whitespace": ["error", {
+                skipComments: true,
+            }],
+
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": ["error", {
+              args: "all",
+              caughtErrors: "all",
+              ignoreRestSiblings: true,
+              argsIgnorePattern: "^_",
+            }],
+        },
+    },
+    ...compat.extends("plugin:@angular-eslint/template/recommended").map(config => ({
+        ...config,
+        files: ["**/*.html"],
+    })),
+    {
+        files: ["**/*.html"],
+        plugins: {
+            "@angular-eslint/template": angularEslintTemplate,
+        },
+
+        languageOptions: {
+            parser: parser,
+        },
+
+        rules: {
+            "@angular-eslint/template/prefer-self-closing-tags": "error",
+        },
+    },
+    {
+        files: ["**/*.html"],
+        rules: {
+            "@typescript-eslint/ban-ts-comment": "off",
+        }
+    }
+
+];
