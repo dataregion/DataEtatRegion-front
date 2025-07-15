e8623bfb8ec4581e348e259397d2836debd18351
Sylvain Jolivet <sylvain.jolivet@sib.fr>
2025-07-15 12:06:53 +0200

    ajout route register

--- apps/budget-dataetat/src/app/app.routes.ts
+++ apps/budget-dataetat/src/app/app.routes.ts
@@ -1,14 +1,24 @@
-import { Routes } from '@angular/router';
+import { CanActivateFn, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { canActivateAuthRole } from './guards/auth-role.guard';
 import { resolveFinancialData } from './resolvers/financial-data.resolver';
 import { resolveMarqueBlancheParsedParams } from './resolvers/marqueblanche-parsed-params.resolver';
 import { Profil } from 'apps/common-lib/src/lib/models/profil.enum.model';
 import { TermOfUseComponent } from 'apps/common-lib/src/lib/components/term-of-use/term-of-use.component';
+import { inject } from '@angular/core';
+import Keycloak from 'keycloak-js';
+
 
 export const profiles_required_for_upload_page = [Profil.ADMIN];
 export const profiles_required_for_tags_page = [Profil.USERS];
 export const profiles_required_for_demarches = [Profil.USERS];
+
+
+const registerRedirect: CanActivateFn = () => {
+    const keycloak = inject(Keycloak);
+    keycloak.register()
+    return false;
+}
 
 export const routes: Routes = [
     {
@@ -22,12 +32,12 @@ export const routes: Routes = [
             mb_parsed_params: resolveMarqueBlancheParsedParams
         }
     },
-    //      {
-    //     path: 'aide',
-    //     component: HelpComponent,
-    //     canActivate: [keycloakAuthGuardCanActivate],
-    //     runGuardsAndResolvers: 'always',
-    //   },
+    {
+        path: 'register',
+        title: "S'enregistrer",
+        canActivate: [registerRedirect],
+        component: HomeComponent
+    },
     {
         path: 'cgu',
         component: TermOfUseComponent,
