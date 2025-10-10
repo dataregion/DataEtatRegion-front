d9c8f50aa5178ce3de820b6b95b185254adf961a
Romain Gautier <romain.gautier@sib.fr>
2025-10-10 14:43:06 +0200

    [FEAT] Restaure l'export vers grist

--- apps/budget-dataetat/src/app/components/home/table-toolbar/services/table-export.service.ts
+++ apps/budget-dataetat/src/app/components/home/table-toolbar/services/table-export.service.ts
@@ -4,37 +4,41 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { FinancialDataModel } from '@models/financial/financial-data.models';
 import { ColonnesService } from '@services/colonnes.service';
-import { ExportDataService } from 'apps/appcommon/src/lib/export-data.service';
+import { DisplayedOrderedColumn, ExportDataService } from 'apps/appcommon/src/lib/export-data.service';
 import { SearchDataService } from '@services/search-data.service';
 import { MatomoTracker } from 'ngx-matomo-client';
+import { BudgetToGristService } from 'apps/clients/budget/api/budgetToGrist.service';
+import { GristDataModel } from 'apps/clients/budget/model/gristDataModel';
+import { AlertService } from 'apps/common-lib/src/public-api';
 
 const LIMITE_EXPORT = 6_000;
 
 @Injectable()
 export class TableExportService {
-  
   private _destroyRef = inject(DestroyRef);
   private _datePipe = inject(DatePipe);
   private _colonnesService = inject(ColonnesService);
   private _tracker = inject(MatomoTracker);
   private _exportDataService = inject(ExportDataService);
   private _searchDataService = inject(SearchDataService);
+  private _alertService = inject(AlertService);
+
+  private _budgetTogrist = inject(BudgetToGristService);
 
   // Signaux pour l'état de l'export
   private _isExporting = signal(false);
-  private _total = signal(0)
-  private _currentCount = signal(0)
-  
-  
+  private _total = signal(0);
+  private _currentCount = signal(0);
+
   public readonly isExporting = this._isExporting.asReadonly();
   public readonly total = this._total.asReadonly();
   public readonly currentCount = this._currentCount.asReadonly();
-  
+
   public readonly isOverExportLimit = computed(() => {
     const totaux = this._searchDataService.total();
     return (totaux?.total ?? 0) > LIMITE_EXPORT;
   });
-  
+
   public readonly currentResultsCount = computed(() => {
     const current_results = this._searchDataService.searchResults() as FinancialDataModel[];
     return current_results?.length ?? 0;
@@ -44,10 +48,12 @@ export class TableExportService {
     const isSearching = this._searchDataService.searchInProgress();
     return this.isOverExportLimit() || isSearching || this.isExporting();
   });
-  
+
   public readonly exportTooltip = computed(() => {
     if (!this.isOverExportLimit()) return '';
-    return `Le téléchargement est limité à ${LIMITE_EXPORT.toLocaleString('fr-FR')} lignes. Veuillez affiner votre recherche.`;
+    return `Le téléchargement est limité à ${LIMITE_EXPORT.toLocaleString(
+      'fr-FR'
+    )} lignes. Veuillez affiner votre recherche.`;
   });
 
   /**
@@ -55,9 +61,9 @@ export class TableExportService {
    */
   private _generateFilename(extension: string): string {
     const searchParams = this._searchDataService.searchParams();
-    
+
     let filename = `${this._datePipe.transform(new Date(), 'yyyyMMdd')}_export`;
-    
+
     if (searchParams?.locations) {
       const locations = searchParams.locations;
       filename += '_' + locations[0].type?.toLowerCase() + '-';
@@ -66,16 +72,55 @@ export class TableExportService {
         .map((loc) => loc.code)
         .join('-');
     }
-    
+
     if (searchParams?.bops) {
       const bops = searchParams.bops;
-      filename += '_bops-' + bops
-        .filter((bop) => bop.code)
-        .map((bop) => bop.code)
-        .join('-');
+      filename +=
+        '_bops-' +
+        bops
+          .filter((bop) => bop.code)
+          .map((bop) => bop.code)
+          .join('-');
     }
 
     return filename + '.' + extension;
+  }
+
+  private _loadMoreIfNecessary(callback: () => void): boolean {
+    let notifyDoEarlyReturn = false;
+    const current_results = this._searchDataService.searchResults() as FinancialDataModel[];
+    const pagination = this._searchDataService.pagination();
+
+    if (current_results.length < LIMITE_EXPORT && pagination?.has_next) {
+      const loadMore$ = this._searchDataService.loadMore();
+      loadMore$?.pipe(takeUntilDestroyed(this._destroyRef)).subscribe({
+        next: () => callback(),
+        error: () => this._isExporting.set(false)
+      });
+      notifyDoEarlyReturn = true;
+    }
+
+    return notifyDoEarlyReturn;
+  }
+
+  private exportColumns(allColumns: boolean) {
+    const selected_colonnes = this._colonnesService.selectedColonnesTable();
+    const all_colonnes = this._colonnesService.allColonnesTable();
+    const colonnes = allColumns ? all_colonnes : selected_colonnes;
+    const export_colonnes = colonnes.map((col) => ({
+      columnLabel: col.label,
+      displayed: true
+    } as DisplayedOrderedColumn));
+    return export_colonnes
+  }
+  
+  private _initExporting() {
+    const current_results = this._searchDataService.searchResults() as FinancialDataModel[];
+    const totaux = this._searchDataService.total();
+
+    this._isExporting.set(true);
+    this._total.set(totaux?.total ?? 0);
+    this._currentCount.set(current_results.length);
   }
 
   /**
@@ -83,43 +128,19 @@ export class TableExportService {
    */
   public downloadData(format: string, allColumns: boolean): void {
     const current_results = this._searchDataService.searchResults() as FinancialDataModel[];
-    const selected_colonnes = this._colonnesService.selectedColonnesTable();
-    const totaux = this._searchDataService.total();
-    const pagination = this._searchDataService.pagination();
-    const all_colonnes = this._colonnesService.allColonnesTable();
 
-    this._isExporting.set(true);
-    this._total.set(totaux?.total ?? 0)
-    this._currentCount.set(current_results.length)
-    
-    // Si on n'a pas toutes les données et qu'il y en a plus à charger
-    if (current_results.length < LIMITE_EXPORT && pagination?.has_next) {
-      const loadMore$ = this._searchDataService.loadMore();
-      loadMore$
-        ?.pipe(takeUntilDestroyed(this._destroyRef))
-        .subscribe({
-          next: () => this.downloadData(format, allColumns),
-          error: () => this._isExporting.set(false)
-        });
-      return;
-    }
+    this._initExporting();
+
+    const callback = () => this.downloadData(format, allColumns);
+    const shouldEarlyReturn = this._loadMoreIfNecessary(callback);
+    if (shouldEarlyReturn) return;
 
     try {
-      // Tracking Matomo
-      this._tracker.trackEvent("Export", "Click", format, undefined, { "allColumns": allColumns });
+      this._tracker.trackEvent('Export', 'Click', format, undefined, { allColumns: allColumns });
 
-      const colonnes = allColumns ? all_colonnes : selected_colonnes;
-      const export_colonnes = colonnes.map(col => ({
-        columnLabel: col.label,
-        displayed: true
-      }));
+      const export_colonnes = this.exportColumns(allColumns)
+      const blob = this._exportDataService.getBlob(current_results ?? [], format, export_colonnes);
 
-      const blob = this._exportDataService.getBlob(
-        current_results ?? [],
-        format,
-        export_colonnes,
-      );
-      
       if (blob) {
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
@@ -141,26 +162,39 @@ export class TableExportService {
    * Exporte les données vers Grist
    */
   public exportToGrist(allColumns: boolean): void {
-    if (this.isExportDisabled()) {
-      return;
-    }
-
-    this._isExporting.set(true);
     
+    this._initExporting();
+
+    const callback = () => this.exportToGrist(allColumns);
+    const shouldEarlyReturn = this._loadMoreIfNecessary(callback);
+    if (shouldEarlyReturn) return;
+
     try {
-      // Tracking Matomo
-      this._tracker.trackEvent("Export", "Click", "grist", undefined, { "allColumns": allColumns });
-      
-      // TODO: Implémenter l'export Grist quand disponible
-      console.warn('[TableExportService] exportToGrist non implémenté', { allColumns });
-      
-      // Simulation d'un délai pour l'export
-      setTimeout(() => {
-        this._isExporting.set(false);
-      }, 1000);
-      
+      this._tracker.trackEvent('Export', 'Click', 'grist', undefined, { allColumns: allColumns });
+
+      const export_colonnes = this.exportColumns(allColumns);
+      const current_results = this._searchDataService.searchResults() as FinancialDataModel[];
+      const data_to_export = this._exportDataService.getDataToExport(current_results, export_colonnes);
+
+      const data = {data: data_to_export} as GristDataModel;
+      this._budgetTogrist.postBugdetToGrist(data)
+          .pipe(takeUntilDestroyed(this._destroyRef))
+          .subscribe(
+              {
+                next:() => {
+                  this._alertService.openAlertSuccess(`Les données ont bien été transférées dans votre espace Grist.`);
+                },
+                error: (err: Error) => {
+                  this._alertService.openAlert("error", err, 8);
+                  this._isExporting.set(false);
+                },
+                complete:() => {
+                  this._isExporting.set(false);
+                }
+              }
+          )
     } catch (error) {
-      console.error('Erreur lors de l\'export vers Grist:', error);
+      console.error("Erreur lors de l'export vers Grist:", error);
       this._isExporting.set(false);
     }
   }
