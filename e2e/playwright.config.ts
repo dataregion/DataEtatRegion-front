92ba953683c6c5cfaf8ff3744bbe4d1e5aa1e913
Romain Gautier <romain.gautier@sib.fr>
2026-02-20 11:21:13 +0100

    [CHORE] passage des TO de 5s à 10s pour les tests e2e

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -23,7 +23,7 @@ const config: PlaywrightTestConfig = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 10000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -38,7 +38,7 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 5000,
+    actionTimeout: 10000,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     testIdAttribute: 'data-test-id'
